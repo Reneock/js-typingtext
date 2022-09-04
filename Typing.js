ea9e49bb42bbedd3ddddc4877fa7008ddadbbb78
Irene Ockrah <ostrene33@gmail.com>
2022-09-04 00:12:34 -0700

    using typed js for typing effect

---
+++ Typing.js
@@ -0,0 +1,8 @@
+var options = {
+  strings: ['Irene', 'a Junior Developer', 'at your service'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+};
+
+var typed = new Typed ('.typing', options);
